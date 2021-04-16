99d34e306cde4c254a6f1f191733c5047404f46d
Patrick <patrick.chapple28@icloud.com>
2021-04-16 00:21:41 -0600

    add backend controller and route for creating a new product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require("../Models/product");
 const asyncHandler = require("express-async-handler");
+const { restart } = require("nodemon");
 
 const getProducts = asyncHandler(async (req, res) => {
   let products = await Product.find();
@@ -45,4 +46,29 @@ const postProductReview = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getProducts, getProductById, postProductReview };
+const createNewProduct = asyncHandler(async (req, res) => {
+  let newProduct = {
+    user: req.user._id,
+    name: "createdName",
+    image: "/images/noImage.png",
+    category: "createdCategory",
+    subcategory: "createdSubcategory",
+    description: "createdDescription",
+    reviews: [],
+  };
+  try {
+    await Product.insertMany([newProduct]);
+    res.send("Product Created");
+  } catch (err) {
+    console.error(err);
+    res.status(400);
+    res.send("Creation of new product failed");
+  }
+});
+
+module.exports = {
+  getProducts,
+  getProductById,
+  postProductReview,
+  createNewProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,12 +4,14 @@ const {
   getProducts,
   getProductById,
   postProductReview,
+  createNewProduct,
 } = require("../controllers/productController");
-const { protect } = require("../middleware/authMiddleware");
+const { protect, protectAdmin } = require("../middleware/authMiddleware");
 
 router.route("/").get(getProducts);
 
 router.route("/:id").get(getProductById);
 router.route("/:id/review").post(protect, postProductReview);
+router.route("/create").post(protectAdmin, createNewProduct);
 
 module.exports = router;
